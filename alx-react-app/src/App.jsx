926b1f3dec48430c6c545748ee0a833d2d88f4a7
Jude Zambarakji <judezambarakji@gmail.com>
2024-08-12 14:56:50 +0300

    Changed UserProfile component information

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
       <MainContent />
       <Footer />
 
-      <UserProfile name="Jude" age="33" bio="Software Engineer" />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
     
   )
