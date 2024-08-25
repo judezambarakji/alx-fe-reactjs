83cd34297bba5f5ff3b9de029fc11c52f8db3426
Jude Zambarakji <judezambarakji@gmail.com>
2024-08-25 17:52:34 +0300

    Updated correct UserProfile file.

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,22 +1,26 @@
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
+// Import React and the useContext hook from the 'react' library
+import React, { useContext } from 'react';
 
-  export default UserProfile
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
+// Import the UserContext we created in UserContext.js
+import UserContext from './UserContext';
 
-  export default UserProfile
+// Define the UserProfile component as a function
+const UserProfile = () => {
+  // Use the useContext hook to access the userData from UserContext
+  const userData = useContext(UserContext);
+
+  // Return the JSX that represents our component's structure
+  return (
+    <div>
+      {/* Display the user's name, accessing it from the context */}
+      <h2>{userData.name}</h2>
+      {/* Display the user's age, accessing it from the context */}
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{userData.age}</span></p>
+      {/* Display the user's bio, accessing it from the context */}
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+};
+
+// Export the UserProfile component so it can be used in other parts of our application
+export default UserProfile;
